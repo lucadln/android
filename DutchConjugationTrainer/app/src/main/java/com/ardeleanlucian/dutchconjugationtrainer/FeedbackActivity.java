619eebd0eedc87df7ace42de43a25eb67f29979d
lucadln <lucyan.ardelean@gmail.com>
2017-09-20 20:58:28 +0300

    DCT21: Performance feedback: - gave up the idea of using fragments. Will use either one activity with conditional elements or multiple activities.

--- DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/FeedbackActivity.java
+++ DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/FeedbackActivity.java
@@ -1,12 +1,7 @@
 package com.ardeleanlucian.dutchconjugationtrainer;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by ardelean on 9/9/17.
@@ -23,18 +18,5 @@ public class FeedbackActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("TEMP_Feedback");
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new FeedbackActivity.FeedbackFragment())
-                .commit();
-    }
-
-    public static class FeedbackFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.article_view, container, false);
-        }
     }
 }
