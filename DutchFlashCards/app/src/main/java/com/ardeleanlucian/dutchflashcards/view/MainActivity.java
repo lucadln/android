661d0d77b25657c8053e4715841258a18763afb8
lucadln <lucyan.ardelean@gmail.com>
2017-10-08 14:31:50 +0300

    DFC1 Refactor the application: Added the layout variables in the MainActivity.

--- DutchFlashCards/app/src/main/java/com/ardeleanlucian/dutchflashcards/view/MainActivity.java
+++ DutchFlashCards/app/src/main/java/com/ardeleanlucian/dutchflashcards/view/MainActivity.java
@@ -1,12 +1,53 @@
 package com.ardeleanlucian.dutchflashcards.view;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.ardeleanlucian.dutchflashcards.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView word;
+    TextView wordTranslation;
+    Button previous;
+    Button next;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        word = (TextView) findViewById(R.id.word);
+        wordTranslation = (TextView) findViewById(R.id.word_translation);
+        previous = (Button) findViewById(R.id.previous);
+        next = (Button) findViewById(R.id.next);
+
+        String TEMP_DIFF = "Temp"; // Stub - to use a get_difficulty() method @TODO
+        toolbar.setTitle("Difficulty: " + TEMP_DIFF);
+        setSupportActionBar(toolbar);
+
+        previous.setOnClickListener(onClickPrevious);
+        next.setOnClickListener(onClickNext);
     }
+
+    private final View.OnClickListener onClickPrevious = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // @TODO
+        }
+    };
+
+    private final View.OnClickListener onClickNext = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // @TODO
+        }
+    };
 }
