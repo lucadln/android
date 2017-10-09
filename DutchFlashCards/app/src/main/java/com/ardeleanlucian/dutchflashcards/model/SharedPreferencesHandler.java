0de9c8d3ef773fb8a8bce01faad85988c371c1ad
YONDER\luciana <lucian.ardelean@tss-yonder.com>
2017-10-09 14:54:57 +0300

    DFC1 Refactor the application: refactored the SharedPreferencesHandler class.

--- DutchFlashCards/app/src/main/java/com/ardeleanlucian/dutchflashcards/model/SharedPreferencesHandler.java
+++ DutchFlashCards/app/src/main/java/com/ardeleanlucian/dutchflashcards/model/SharedPreferencesHandler.java
@@ -17,12 +17,6 @@ public class SharedPreferencesHandler {
     private final String LATEST_MEDIUM_WORD_KEY = "com.ardeleanlucian.dutchflashcards.latest_medium_word";
     private final String LATEST_HARD_WORD_KEY = "com.ardeleanlucian.dutchflashcards.latest_hard_word";
 
-    private String primaryLanguage;
-    private String difficultyLevel;
-    private String latestEasyWord;
-    private String latestMediumWord;
-    private String latestHardWord;
-
     /**
      * Constructor method
      * @param context
@@ -33,75 +27,50 @@ public class SharedPreferencesHandler {
                 "com.ardeleanlucian.dutchflashcards", Context.MODE_PRIVATE);
     }
 
-
     /**
      * Methods to read values from the SharedPreferences.
      */
-    public void readPrimaryLanguage() {
-        primaryLanguage = prefs.getString(PRIMARY_LANGUAGE_KEY, "dutch");
+    public String readPrimaryLanguage() {
+        return prefs.getString(PRIMARY_LANGUAGE_KEY, "dutch");
     }
 
-    public void readDifficultyLevel() {
-        difficultyLevel = prefs.getString(DIFFICULTY_LEVEL_KEY, "easy");
+    public String readDifficultyLevel() {
+        return prefs.getString(DIFFICULTY_LEVEL_KEY, "easy");
     }
 
-    public void readLatestEasyWord() {
-        latestEasyWord = prefs.getString(LATEST_EASY_WORD_KEY, "test"); //@TODO make this not be hardcoded
+    public String readLatestEasyWord() {
+        return prefs.getString(LATEST_EASY_WORD_KEY, "test"); //@TODO make this not be hardcoded
     }
 
-    public void readLatestMediumWord() {
-        latestMediumWord = prefs.getString(LATEST_MEDIUM_WORD_KEY, "test"); //@TODO make this not be hardcoded
+    public String readLatestMediumWord() {
+        return prefs.getString(LATEST_MEDIUM_WORD_KEY, "test"); //@TODO make this not be hardcoded
     }
 
-    public void readLatestHardWord() {
-        latestHardWord = prefs.getString(LATEST_HARD_WORD_KEY, "test"); //@TODO make this not be hardcoded
-    }
-
-    /**
-     * Getters for the SharedPreferences values
-     * @return
-     */
-    public String getPrimaryLanguage() {
-        return primaryLanguage;
-    }
-
-    public String getDifficultyLevel() {
-        return difficultyLevel;
-    }
-
-    public String getLatestEasyWord() {
-        return latestEasyWord;
-    }
-
-    public String getLatestMediumWord() {
-        return latestMediumWord;
-    }
-
-    public String getLatestHardWord() {
-        return latestHardWord;
+    public String readLatestHardWord() {
+        return prefs.getString(LATEST_HARD_WORD_KEY, "test"); //@TODO make this not be hardcoded
     }
 
     /**
-     * Methods to update the current value in the android's Shared Preferences
+     * Methods to update values in the android's Shared Preferences
      * @param newValue
      */
-    public void updatePrimaryLanguage(String newValue) {
+    public void writtePrimaryLanguage(String newValue) {
         prefs.edit().putString(PRIMARY_LANGUAGE_KEY, newValue).apply();
     }
 
-    public void updateDifficultyLevel(String newValue) {
+    public void writteDifficultyLevel(String newValue) {
         prefs.edit().putString(DIFFICULTY_LEVEL_KEY, newValue).apply();
     }
 
-    public void updateLatestEasyWord(String newValue) {
+    public void writteLatestEasyWord(String newValue) {
         prefs.edit().putString(LATEST_EASY_WORD_KEY, newValue).apply();
     }
 
-    public void updateLatestMediumWord(String newValue) {
+    public void writteLatestMediumWord(String newValue) {
         prefs.edit().putString(LATEST_MEDIUM_WORD_KEY, newValue).apply();
     }
     
-    public void updateLatestHardWord(String newValue) {
+    public void writteLatestHardWord(String newValue) {
         prefs.edit().putString(LATEST_HARD_WORD_KEY, newValue).apply();
     }
 }
