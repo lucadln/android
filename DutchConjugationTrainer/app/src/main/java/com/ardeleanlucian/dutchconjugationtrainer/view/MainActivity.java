6654f0145d722139f35c568bef7288a8754459f7
lucadln <lucyan.ardelean@gmail.com>
2017-10-19 20:20:00 +0300

    DCT16 Refactor code: - added menu items in the toolbar; - added dummy activities; - registered the activities in the android manifest file.

--- DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/MainActivity.java
+++ DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ardeleanlucian.dutchconjugationtrainer.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -102,6 +105,48 @@ public class MainActivity extends AppCompatActivity {
           //@TODO
         }
     };
+
+    /**
+     * Inflate menu in the toolbar
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * Select actions on menu selection
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.settings:
+                Intent displaySettings = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(displaySettings);
+                return true;
+            case R.id.scores:
+                Intent displayScores = new Intent(MainActivity.this, ScoresActivity.class);
+                startActivity(displayScores);
+                return true;
+            case R.id.tense_info:
+                Intent displayTensesInfo = new Intent(MainActivity.this, TensesInfoActivity.class);
+                startActivity(displayTensesInfo);
+                return true;
+            case R.id.about:
+                Intent displayAbout = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(displayAbout);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void initializeLayoutElements() {
         TABLE_LAYOUT = (TableLayout) findViewById(R.id.table_layout);
