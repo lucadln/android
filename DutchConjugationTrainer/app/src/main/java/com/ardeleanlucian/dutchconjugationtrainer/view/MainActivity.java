f99f1dacda545ece806974dd61a62046bd171458
lucadln <lucyan.ardelean@gmail.com>
2017-11-06 20:41:02 +0200

    DCT16 Refactor code: small mod.

--- DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/MainActivity.java
+++ DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/MainActivity.java
@@ -2,7 +2,6 @@ package com.ardeleanlucian.dutchconjugationtrainer.view;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
