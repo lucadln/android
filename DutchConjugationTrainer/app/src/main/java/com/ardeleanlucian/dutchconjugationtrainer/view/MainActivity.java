f6b15cfb374f0288980c9a47b64ce130e62d0704
YONDER\luciana <lucian.ardelean@tss-yonder.com>
2017-11-10 13:04:54 +0200

    DCT16Handler: Separated the declaration and initialization of the layout elements by using a new handler class.

--- DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/MainActivity.java
+++ DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/MainActivity.java
@@ -27,35 +27,6 @@ import static android.view.View.VISIBLE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TableLayout TABLE_LAYOUT;
-
-    private Button NEXT;
-    private Button SKIP;
-
-    private TextView INFINITIVE;
-    private TextView TRANSLATION;
-    private TextView IK;
-    private TextView JIJ;
-    private TextView HIJ;
-    private TextView WIJ;
-    private TextView JULLIE;
-    private TextView ZIJ;
-    private TextView IK_VERB_TEXT;
-    private TextView JIJ_VERB_TEXT;
-    private TextView HIJ_VERB_TEXT;
-    private TextView WIJ_VERB_TEXT;
-    private TextView JULLIE_VERB_TEXT;
-    private TextView ZIJ_VERB_TEXT;
-
-    private EditText IK_VERB_FIELD;
-    private EditText JIJ_VERB_FIELD;
-    private EditText HIJ_VERB_FIELD;
-    private EditText WIJ_VERB_FIELD;
-    private EditText JULLIE_VERB_FIELD;
-    private EditText ZIJ_VERB_FIELD;
-
-    private Spinner spinner;
-
     private Verb currentVerb;
     private MainController controller;
     private boolean readOnlyMode;
@@ -75,7 +46,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeLayoutElements();
+
+        MainActivityHandler mainActivityHandler = new MainActivityHandler(this);
+
+        mainActivityHandler.initializeLayoutElements();
 
         controller = new MainController(this);
 
@@ -85,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         // Set up the spinner
-        spinner = (Spinner) findViewById(R.id.spinner);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(new SpinnerAdapter(toolbar.getContext(), Score.tenses));
         spinner.setSelection(controller.obtainSpinnerIndex());
         spinner.setOnItemSelectedListener(onSpinnerSelection);
@@ -99,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         resetConjugationSectionVisibility(
                 readOnlyMode = controller.obtainReadOnlyPreference(),
                 controller.obtainShowTranslationPreference());
+        setOnFocusChangeListenerForEditTexts();
     }
 
     /**
@@ -304,57 +279,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    /**
-     * Initialize layout elements
-     */
-    private void initializeLayoutElements() {
-        TABLE_LAYOUT = (TableLayout) findViewById(R.id.table_layout);
-
-        NEXT = (Button) findViewById(R.id.next);
-        SKIP = (Button) findViewById(R.id.skip);
-
-        INFINITIVE = (TextView) findViewById(R.id.infinitive);
-        TRANSLATION = (TextView) findViewById(R.id.translation);
-        IK = (TextView) findViewById(R.id.ik);
-        JIJ = (TextView) findViewById(R.id.jij);
-        HIJ = (TextView) findViewById(R.id.hij);
-        WIJ = (TextView) findViewById(R.id.wij);
-        JULLIE = (TextView) findViewById(R.id.jullie);
-        ZIJ = (TextView) findViewById(R.id.zij);
-        IK_VERB_TEXT = (TextView) findViewById(R.id.ik_verb_text);
-        JIJ_VERB_TEXT = (TextView) findViewById(R.id.jij_verb_text);
-        HIJ_VERB_TEXT = (TextView) findViewById(R.id.hij_verb_text);
-        WIJ_VERB_TEXT = (TextView) findViewById(R.id.wij_verb_text);
-        JULLIE_VERB_TEXT = (TextView) findViewById(R.id.jullie_verb_text);
-        ZIJ_VERB_TEXT = (TextView) findViewById(R.id.zij_verb_text);
-
-        IK_VERB_FIELD = (EditText) findViewById(R.id.ik_verb_field);
-        JIJ_VERB_FIELD = (EditText) findViewById(R.id.jij_verb_field);
-        HIJ_VERB_FIELD = (EditText) findViewById(R.id.hij_verb_field);
-        WIJ_VERB_FIELD = (EditText) findViewById(R.id.wij_verb_field);
-        JULLIE_VERB_FIELD = (EditText) findViewById(R.id.jullie_verb_field);
-        ZIJ_VERB_FIELD = (EditText) findViewById(R.id.zij_verb_field);
-
-        IK_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
-        JIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
-        HIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
-        WIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
-        JULLIE_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
-        ZIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
-    }
-
-    /**
-     * Method to clear all values from the layout fields
-     */
-    private void clearFields() {
-        IK_VERB_FIELD.setText( "" );
-        JIJ_VERB_FIELD.setText( "" );
-        HIJ_VERB_FIELD.setText( "" );
-        WIJ_VERB_FIELD.setText( "" );
-        JULLIE_VERB_FIELD.setText( "" );
-        ZIJ_VERB_FIELD.setText( "" );
-    }
-
     public int getNumberOfFilledEditTexts() {
         int count = 0;
         if (IK_VERB_FIELD.getVisibility() == View.GONE) {
@@ -378,6 +302,15 @@ public class MainActivity extends AppCompatActivity {
         return count;
     }
 
+    setOnFocusChangeListenerForEditTexts() {
+        IK_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
+        JIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
+        HIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
+        WIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
+        JULLIE_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
+        ZIJ_VERB_FIELD.setOnFocusChangeListener(onFocusChangeListener);
+    }
+
     /**
      * Method to set the values for the TextViews in the layout.
      *   Only important in the read-only mode.
@@ -397,65 +330,6 @@ public class MainActivity extends AppCompatActivity {
             WIJ_VERB_TEXT.setText(verb.getVerbConjugation()[spinnerIndex][3]);
             JULLIE_VERB_TEXT.setText(verb.getVerbConjugation()[spinnerIndex][4]);
             ZIJ_VERB_TEXT.setText(verb.getVerbConjugation()[spinnerIndex][5]);
-        }
-    }
-
-    /**
-     * Method to reset the conjugation visibility depending
-     *   on the play mode (read-only or write).
-     *
-     * @param readOnly
-     */
-    private void resetConjugationSectionVisibility(boolean readOnly, boolean showTranslation) {
-        SKIP.setVisibility(VISIBLE);
-        NEXT.setVisibility(GONE);
-        if (showTranslation) {
-            TRANSLATION.setVisibility(VISIBLE);
-        } else {
-            TRANSLATION.setVisibility(INVISIBLE);
-        }
-        if (readOnly) {
-            IK.setVisibility(VISIBLE);
-            JIJ.setVisibility(INVISIBLE);
-            HIJ.setVisibility(INVISIBLE);
-            WIJ.setVisibility(INVISIBLE);
-            JULLIE.setVisibility(INVISIBLE);
-            ZIJ.setVisibility(INVISIBLE);
-
-            IK_VERB_TEXT.setVisibility(INVISIBLE);
-            JIJ_VERB_TEXT.setVisibility(INVISIBLE);
-            HIJ_VERB_TEXT.setVisibility(INVISIBLE);
-            WIJ_VERB_TEXT.setVisibility(INVISIBLE);
-            JULLIE_VERB_TEXT.setVisibility(INVISIBLE);
-            ZIJ_VERB_TEXT.setVisibility(INVISIBLE);
-
-            IK_VERB_FIELD.setVisibility(GONE);
-            JIJ_VERB_FIELD.setVisibility(GONE);
-            HIJ_VERB_FIELD.setVisibility(GONE);
-            WIJ_VERB_FIELD.setVisibility(GONE);
-            JULLIE_VERB_FIELD.setVisibility(GONE);
-            ZIJ_VERB_FIELD.setVisibility(GONE);
-        } else {
-            IK.setVisibility(VISIBLE);
-            JIJ.setVisibility(VISIBLE);
-            HIJ.setVisibility(VISIBLE);
-            WIJ.setVisibility(VISIBLE);
-            JULLIE.setVisibility(VISIBLE);
-            ZIJ.setVisibility(VISIBLE);
-
-            IK_VERB_TEXT.setVisibility(GONE);
-            JIJ_VERB_TEXT.setVisibility(GONE);
-            HIJ_VERB_TEXT.setVisibility(GONE);
-            WIJ_VERB_TEXT.setVisibility(GONE);
-            JULLIE_VERB_TEXT.setVisibility(GONE);
-            ZIJ_VERB_TEXT.setVisibility(GONE);
-
-            IK_VERB_FIELD.setVisibility(VISIBLE);
-            JIJ_VERB_FIELD.setVisibility(VISIBLE);
-            HIJ_VERB_FIELD.setVisibility(VISIBLE);
-            WIJ_VERB_FIELD.setVisibility(VISIBLE);
-            JULLIE_VERB_FIELD.setVisibility(VISIBLE);
-            ZIJ_VERB_FIELD.setVisibility(VISIBLE);
         }
     }
 }
