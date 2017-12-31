3cba07f3f37e80dbf4c33e264853fa7ba21c672b
lucadln <lucyan.ardelean@gmail.com>
2017-12-31 17:09:36 +0200

    Added more picture sizes for the feedback activity.

--- DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/FeedbackActivity.java
+++ DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/view/FeedbackActivity.java
@@ -1,7 +1,11 @@
 package com.ardeleanlucian.dutchconjugationtrainer.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ardeleanlucian.dutchconjugationtrainer.R;
@@ -12,18 +16,19 @@ import com.ardeleanlucian.dutchconjugationtrainer.R;
 
 public class FeedbackActivity extends AppCompatActivity {
     private TextView FEEDBACK_MESSAGE;
+    private Button BACK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Add an action bar with navigation.
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setTitle("TEMP_Feedback");
+        // Hide action bar
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_feedback);
         FEEDBACK_MESSAGE = (TextView) findViewById(R.id.feedback_message);
+        BACK = (Button) findViewById(R.id.back);
 
         String feedbackMotivation = getIntent().getExtras().getString("feedbackMotivation");
 
@@ -39,12 +44,26 @@ public class FeedbackActivity extends AppCompatActivity {
         } else if (feedbackMotivation.equals("ratingOver90")) {
             String tense = getIntent().getExtras().getString("tense");
             FEEDBACK_MESSAGE.setText("Your score for the tense " + tense + " is over 90!");
-        } else if (feedbackMotivation.equals("ratingUnder10")) {
-            String tense = getIntent().getExtras().getString("tense");
-            FEEDBACK_MESSAGE.setText("Your score for the tense " + tense + " is under 10!");
         } else if (feedbackMotivation.equals("ratingUnder40")) {
             String tense = getIntent().getExtras().getString("tense");
             FEEDBACK_MESSAGE.setText("Your score for the tense " + tense + " is under 40!");
+        } else if (feedbackMotivation.equals("ratingUnder10")) {
+            String tense = getIntent().getExtras().getString("tense");
+            FEEDBACK_MESSAGE.setText("Your score for the tense " + tense + " is under 10!");
         }
+
+        BACK.setOnClickListener(onClickBack);
     }
+
+    /**
+     * Actions to be taken when clicking on the 'Back' button
+     */
+    private final View.OnClickListener onClickBack = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent displayMainActivity= new Intent(
+                    FeedbackActivity.this, MainActivity.class);
+            startActivity(displayMainActivity);
+        }
+    };
 }
