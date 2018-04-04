10a27af243568dedcd4bc831f2f36f9b12b205dc
lucadln <lucyan.ardelean@gmail.com>
2018-04-04 22:50:32 +0300

    DCT33: Create algorithm for 'almost' correct answers

--- DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/model/UserAnswer.java
+++ DutchConjugationTrainer/app/src/main/java/com/ardeleanlucian/dutchconjugationtrainer/model/UserAnswer.java
@@ -9,7 +9,7 @@ public class UserAnswer {
     private Verb verb;
     private int spinnerPosition;
     private int numberOfConjugatedPersons = 0;
-    private boolean verbConjugationCorrectness = true;
+    private String verbConjugationCorrectness = "correct";
 
     public UserAnswer(int spinnerPosition, Verb verb) {
         this.verb = verb;
@@ -20,22 +20,38 @@ public class UserAnswer {
      * Checks whether an answer (a conjugation of a certain person in a certain tense) is correct
      *
      * @param conjugationIndex
-     * @param answer
+     * @param userAnswer
      * @return
      */
-    public boolean isAnswerInputCorrect(int conjugationIndex, String answer) {
-        if (!answer.equals("")) {
-            numberOfConjugatedPersons++;
-            if ((answer.equalsIgnoreCase(verb.getVerbConjugation()[spinnerPosition][conjugationIndex]))
-                    || (answer.equalsIgnoreCase(
-                        verb.getVerbConjugation()[spinnerPosition][conjugationIndex]
-                                .replace("ï", "i")))) {
-                return true;
-            } else {
-                return false;
-            }
+    public String isAnswerInputCorrect(int conjugationIndex, String userAnswer) {
+        if (userAnswer.equals("")) {
+            // No input. Move on.
+            return "correct";
         } else {
-            return true;
+            numberOfConjugatedPersons++;
+            userAnswer = userAnswer.toLowerCase();
+            String correctAnswer = verb.getVerbConjugation()[spinnerPosition][conjugationIndex];
+            int differentCharsCount = 0;
+            int stringLength = 0;
+
+            if ((stringLength = userAnswer.length()) != correctAnswer.length()) {
+                // The strings are not equal
+                return "incorrect";
+            } else {
+                for (int i = 0; i < stringLength; i++) {
+                    if (userAnswer.charAt(i) != correctAnswer.charAt(i)) {
+                        differentCharsCount++;
+                    }
+                }
+                if (differentCharsCount == 0) {
+                    return "correct";
+                } else if (differentCharsCount == 1) {
+                    return "almost";
+                }
+                else {
+                    return "incorrect";
+                }
+            }
         }
     }
 
@@ -45,13 +61,20 @@ public class UserAnswer {
      *
      * @param answerCorrectness
      */
-    public void updateConjugationStatus(boolean answerCorrectness) {
+    public void updateConjugationStatus(String answerCorrectness) {
         // If the verb is correctly conjugated so far
-        //   then check the current answer. If not then
-        //   it doesn't matter what the current answer is since
-        //   the verb conjugation is already wrong overall.
-        if (verbConjugationCorrectness == true) {
+        //   the conjugation can change depending on the last input
+        if (verbConjugationCorrectness.equals("correct")) {
             verbConjugationCorrectness = answerCorrectness;
+        // If the verb is conjugated almost correctly until now...
+        } else if (verbConjugationCorrectness.equals("almost")) {
+            if (answerCorrectness.equals("incorrect")) {
+                // If the latest input is wrong the whole conjugation is wrong
+                verbConjugationCorrectness = answerCorrectness;
+            }
+        } else {
+            // If the verb is already wrongly conjugated
+            //   a correct conjugation won't do any difference
         }
     }
 
@@ -67,7 +90,7 @@ public class UserAnswer {
      * @return a boolean value which represents the correctness
      *         of the conjugation of a verb in a certain tense
      */
-    public boolean isVerbCorrectlyConjugated() {
+    public String getConjugationCorrectnessForVerb() {
         return verbConjugationCorrectness;
     }
 }
